ab4a380ed2162a860d4d479c5bbe0e23d6da158c
adamjatim <adamrahmatilahi@gmail.com>
2022-12-11 23:45:25 +0700

    Update TABLE

--- assigment_03/perpustakaan.sql
+++ assigment_03/perpustakaan.sql
@@ -10,6 +10,15 @@ CREATE TABLE Anggota (
     PRIMARY KEY (id_anggota)
 );
 
+CREATE TABLE Petugas (
+    id_petugas int NOT NULL,
+    nama_petugas VARCHAR(255),
+    jabatan_petugas(255),
+    no_telp_petugas int(20),
+    alamat_petugas VARCHAR(255),
+    PRIMARY KEY (id_petugas)
+);
+
 CREATE TABLE Buku (
     id_buku int NOT NULL,
     kode_buku VARCHAR(8),
@@ -22,9 +31,21 @@ CREATE TABLE Buku (
 );
 
 CREATE TABLE Peminjaman (
-    id_peminjaman,
-    tanggal_pinjam,
-    tanggal_kembali,
+    id_peminjaman int NOT NULL,
+    tanggal_pinjam date,
+    tanggal_kembali date,
+    fk_id_buku int,
+    fk_id_anggota int,
+    fk_id_petugas int,
+    PRIMARY KEY (id_peminjaman),
+    FOREIGN KEY (fk_id_buku) REFERENCES Buku(id_buku),
+    FOREIGN KEY (fk_id_anggota) REFERENCES Anggota(id_anggota),
+    FOREIGN KEY (fk_id_petugas) REFERENCES Petugas(id_petugas)
+);
+
+CREATE TABLE Pengembalian (
+    id_pengembalian int NOT NULL,
+    denda ,
     fk_id_buku,
     fk_id_anggota,
     fk_id_petugas
