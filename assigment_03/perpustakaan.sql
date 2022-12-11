485fc26730a956f802a90da330992fb5dc2c1d4f
adamjatim <adamrahmatilahi@gmail.com>
2022-12-11 22:40:00 +0700

    Update TABLE

--- assigment_03/perpustakaan.sql
+++ assigment_03/perpustakaan.sql
@@ -9,3 +9,23 @@ CREATE TABLE Anggota (
     alamat_anggota VARCHAR(255),
     PRIMARY KEY (id_anggota)
 );
+
+CREATE TABLE Buku (
+    id_buku int NOT NULL,
+    kode_buku VARCHAR(8),
+    judul_buku VARCHAR(255),
+    penulisan_buku VARCHAR(255),
+    penerbitan_buku VARCHAR(255),
+    tahun_penerbitan int(8),
+    stok int(8),
+    PRIMARY KEY (id_buku)
+);
+
+CREATE TABLE Peminjaman (
+    id_peminjaman,
+    tanggal_pinjam,
+    tanggal_kembali,
+    fk_id_buku,
+    fk_id_anggota,
+    fk_id_petugas
+);
